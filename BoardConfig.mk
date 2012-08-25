613defe30036d226506eac1efb2b143fc4d863b4
texten <zutexten@live.de>
2012-08-25 02:49:39 +0200

    don't use tslib

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,7 @@
 # WARNING: This line must come *before* including the proprietary
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
+
 USE_CAMERA_STUB := true
 
 
